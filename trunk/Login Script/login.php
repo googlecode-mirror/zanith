17363e80c3ee00a9a951454f213143e2c23df5eb
benmfowler <benmfowler@e5d3bfbe-ed86-11de-9b30-9f9d59cce923>
2009-12-20 18:58:56 +0000

    Uploading Login Files

---
+++ trunk/Login Script/login.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+<form action="index.php?page=logon" method="post">
+<label for="username">Username:</label>
+<input type="text" name="username" />
+<br />
+<label for="password">Password:</label>
+<input type="password" name="password" />
+<br />
+<input type="submit" name="submit" value="Login" />
+</form>
